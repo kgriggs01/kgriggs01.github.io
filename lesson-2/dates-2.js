b06bec97646cf76415ff5e8211ee85088dcedc4a
kgriggs01 <gri18026@byui.edu>
2019-09-27 00:46:42 -0700

    commit

--- lesson-2/dates-2.js
+++ lesson-2/dates-2.js
@@ -1,3 +1,4 @@
-
-
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+var copyright_year = new Date();
+document.getElementById("copyright_year").innerHTML = copyright_year.getFullYear();
