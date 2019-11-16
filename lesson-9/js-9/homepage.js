5fbe1c1f7396f53d137d26450643cb81ceef2586
kgriggs01 <gri18026@byui.edu>
2019-11-15 23:19:18 -0800

    commit

--- lesson-9/js-9/homepage.js
+++ lesson-9/js-9/homepage.js
@@ -5,37 +5,39 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject); // temporary checking for valid response and data parsing
+    console.table(jsonObject); // temporary checking for valid response and data parsing
     const towns = jsonObject.towns;
     for (let i = 0; i < towns.length; i++) {
 
+      if (towns[i].name == 'Preston' || towns[i].name == 'Soda Springs' ||towns[i].name ==  'Fish Haven' ) {
+
+      let town = ["Preston", "Soda Springs", "Fish Haven"]; 
       let card = document.createElement("section");
-
       let townNameHeading = document.createElement("h2");
-      townNameHeading.textContent = towns[i].name;
-      card.appendChild(townNameHeading);
-
       let motto = document.createElement("h3");
-      motto.textContent = towns[i].motto;
-      card.appendChild(motto);
-
-
       let yearFounded = document.createElement("p");
-      yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
-      card.appendChild(yearFounded);
-
-      
       let population = document.createElement("p");
-      population.textContent = "Population: " + towns[i].currentPopulation;
-      card.appendChild(population);
-
       let annualRainfall = document.createElement("p");
+      let image = document.createElement("img");
+
+
+      townNameHeading.textContent = towns[i].name;
+      motto.textContent = towns[i].motto;
+      yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
+      population.textContent = "Population: " + towns[i].currentPopulation;
       annualRainfall.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
+      image.setAttribute("src", "./images/homepagePics/" + towns[i].photo);
+      image.setAttribute("alt", towns[i].photo);
+
+
+      card.appendChild(townNameHeading);
+      card.appendChild(motto);
+      card.appendChild(yearFounded);
+      card.appendChild(population);
       card.appendChild(annualRainfall);
-
-
+      card.appendChild(image);
 
       document.querySelector("div.cards").appendChild(card);
     }
-    
+  }
   });
