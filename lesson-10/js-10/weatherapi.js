ad108bd0b113489b3c901c5af53779c96fc52edd
kgriggs01 <gri18026@byui.edu>
2019-11-22 14:14:54 -0800

    commit

--- lesson-10/js-10/weatherapi.js
+++ lesson-10/js-10/weatherapi.js
@@ -1,13 +1,13 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c5be23148aa1ea915f584c3fbe57e45c";
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c5be23148aa1ea915f584c3fbe57e45c";
 
-fetch(apiURL)
+fetch(weatherURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
 
     document.getElementById('current-weather-condition').textContent = jsObject.weather[0].main;
     document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0);
-    document.getElementById('current-wind-speed').textContent = jsObject.wind.speed;
+    document.getElementById('current-wind-speed').textContent = jsObject.wind.speed.toFixed(1);
     document.getElementById('current-humidity').textContent = jsObject.main.humidity;
     document.getElementById('high-temp').textContent = jsObject.main.temp_max.toFixed(0);
     document.getElementById('low-temp').textContent = jsObject.main.temp_min.toFixed(0);
@@ -32,44 +32,46 @@ fetch(apiURL)
 
   });
 
-  const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=c5be23148aa1ea915f584c3fbe57e45c";
 
-  fetch(apiURL2)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (jsObject) {
-      //console.table(jsonObject); // temporary checking for valid response and data parsing
-     
-      const forecast = jsObject.forecast;
-  
-        for (let i = 0; i < jsObject.list.length; i++) {
-        if (jsObject.list[i].dt_txt.includes('18:00:00')) {
-          console.log(jsObject.list[i].dt_txt);
-          console.log(jsObject.list[i].main.temp.toFixed(0));
-          console.log(jsObject.list[i].weather[0].icon);
-  
-          const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
-          const desc = jsObject.list[i].weather[0].description;
-  
-          let dayContainer = document.createElement("section");
-          let forecastContainer = document.createElement("div");
-          let dayOfWeek = document.createElement("h5");
-          let image = document.createElement("img");
-          let futureTemp = document.createElement("p");
-  
-          dayOfWeek.textContent = jsObject.list[i].dt_txt;
-          image.setAttribute("src", imagesrc);
-          image.setAttribute("alt", desc);
-          futureTemp.textContent = jsObject.list[i].main.temp.toFixed(0) + (' °F');
 
-          dayContainer.appendChild(forecastContainer);
-          forecastContainer.appendChild(dayOfWeek);
-          dayContainer.appendChild(image);
-          forecastContainer.appendChild(futureTemp);
-  
-          document.querySelector("div.dayContainers").appendChild(dayContainer);
-          
-        }
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=c5be23148aa1ea915f584c3fbe57e45c";
+
+fetch(forecastURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsObject) {
+    //console.table(jsonObject); // temporary checking for valid response and data parsing
+
+    const forecast = jsObject.forecast;
+
+    for (let i = 0; i < jsObject.list.length; i++) {
+      if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+        //console.log(jsObject.list[i].dt_txt);
+        //console.log(jsObject.list[i].main.temp.toFixed(0));
+        //console.log(jsObject.list[i].weather[0].icon);
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
+        const desc = jsObject.list[i].weather[0].description;
+
+        let dayContainer = document.createElement("div");
+        let forecastContainer = document.createElement("section");
+        let dayOfWeek = document.createElement("h5");
+        let image = document.createElement("img");
+        let futureTemp = document.createElement("p");
+
+        dayOfWeek.textContent = jsObject.list[i].dt_txt;
+        image.setAttribute("src", imagesrc);
+        image.setAttribute("alt", desc);
+        futureTemp.textContent = jsObject.list[i].main.temp.toFixed(0) + (' °F');
+
+        dayContainer.appendChild(forecastContainer);
+        forecastContainer.appendChild(dayOfWeek);
+        forecastContainer.appendChild(image);
+        forecastContainer.appendChild(futureTemp);
+
+        document.querySelector("div.dayContainers").appendChild(dayContainer);
+
       }
-    });
+    }
+  });
