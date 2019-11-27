d09fc9cfeeb195a13b6b3afbf900d3a794fbc651
kgriggs01 <gri18026@byui.edu>
2019-11-26 23:45:56 -0800

    commit

--- lesson-11/js/onload.js
+++ lesson-11/js/onload.js
@@ -1,6 +1,20 @@
 function prestonBanner() {
 
-if (thisDay == "Friday") {
-    document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+    if (thisDay == "Friday") {
+        document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+    }
 }
+
+function sodaSpringsBanner() {
+
+    if (thisDay == "Friday") {
+        document.getElementById("banner").innerHTML = "Saturday = Soda Springs Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+    }
+}
+
+function fishHavenBanner() {
+
+    if (thisDay == "Friday") {
+        document.getElementById("banner").innerHTML = "Saturday = Fish Haven Pancakes in the Park! 9:00 a.m. Saturday at the city park.";
+    }
 }
