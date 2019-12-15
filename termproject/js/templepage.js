8ed004a0c13a2ce9fdf5ee9474b1750153c25713
kgriggs01 <gri18026@byui.edu>
2019-12-14 23:55:22 -0800

    commit

--- termproject/js/templepage.js
+++ termproject/js/templepage.js
@@ -9,41 +9,38 @@ fetch(requestURL)
     const temples = jsonObject.temples;
     for (let i = 0; i < temples.length; i++) {
 
-      //if (temples[i].name == 'Logan' || temples[i].name == 'Portland' || temples[i].name == 'San Diego' || temples[i].name == 'Star Valley') {
+      if (temples[i].name == 'Logan' || temples[i].name == 'Portland' || temples[i].name == 'San Diego' || temples[i].name == 'Star Valley') {
 
-       // let temple = ["Logan", "Portland", "San Diego", "Star Valley"];
+        let temple = ["Logan", "Portland", "San Diego", "Star Valley"];
         let card = document.createElement("section");
         let templeNameHeading = document.createElement("h3");
-        let photo = document.createElement("h4");
-        let cityStateZip = document.createElement("p");
-        let country = document.createElement("p");
-        let telephone = document.createElement("p");
-        let email = document.createElement("p");
+        let motto = document.createElement("h4");
+        let yearFounded = document.createElement("p");
+        let population = document.createElement("p");
+        let annualRainfall = document.createElement("p");
         let templeInfoDiv = document.createElement("div");
         let templeName = document.createElement("h3");
         let image = document.createElement("img");
 
         templeNameHeading.textContent = temples[i].fullname;
-        streetAddress.textContent = temples[i].streetAddress;
-        cityStateZip.textContent = temples[i].cityStateZip;
-        country.textContent = temples[i].country;
-        telephone.textContent = temples[i].telephone;
-        email.textContent = temples[i].email;
+        motto.textContent = temples[i].motto;
+        yearFounded.textContent = "Year Founded: " + temples[i].yearFounded;
+        population.textContent = "Population: " + temples[i].currentPopulation;
+        annualRainfall.textContent = "Annual Rainfall: " + temples[i].averageRainfall;
         image.setAttribute("src", "./images/" + temples[i].photo);
         image.setAttribute("alt", temples[i].photo);
 
         templeName.appendChild(templeNameHeading);
-        templeInfoDiv.appendChild(streetAddress);
-        templeInfoDiv.appendChild(cityStateZip);
-        templeInfoDiv.appendChild(country);
-        templeInfoDiv.appendChild(telephone);
-        templeInfoDiv.appendChild(email);
+        templeInfoDiv.appendChild(motto);
+        templeInfoDiv.appendChild(yearFounded);
+        templeInfoDiv.appendChild(population);
+        templeInfoDiv.appendChild(annualRainfall);
         card.appendChild(templeName);
         card.appendChild(image);
         card.appendChild(templeInfoDiv);
    
         document.querySelector("div.templeInfo").appendChild(card);
-      //}
+      }
     }
   });
 
